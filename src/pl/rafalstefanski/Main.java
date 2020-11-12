e4a7680340303e82cfbfc9a7cb3661c301d96206
Rafał Stefański <rafal-stefanski@wp.pl>
2020-11-12 16:24:58 +0100

    More on Paths 254 (@MSI)

--- src/pl/rafalstefanski/Main.java
+++ src/pl/rafalstefanski/Main.java
@@ -13,11 +13,20 @@ public class Main {
         Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
 
-        Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
+//        Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
+        Path filePath = Paths.get(".", "files", "SubdirectoryFile.txt");
         printFile(filePath);
 
         filePath = Paths.get("C:\\Users\\RS\\IdeaProjects\\OutThere.txt");
         printFile(filePath);
+
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
+
+        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
+        System.out.println(path2.normalize().toAbsolutePath());
+        printFile(path2.normalize());
+
     }
 
     private static void printFile(Path path) {
