12b259e10fde0952861225a96aca5f3801fbe485
Rafał Stefański <rafal-stefanski@wp.pl>
2020-11-12 14:22:08 +0100

    Filesystem lesson 254 (@MSI)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Paths
 Working with files paths in JAVA.
+
+CAUTION: There is external file, outside Project folder needed to work properly.
+Path to file is here (@MSI) C:\Users\RS\IdeaProjects\OutThere.txt
--- src/pl/rafalstefanski/Main.java
+++ src/pl/rafalstefanski/Main.java
@@ -5,15 +5,19 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
         Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
+
         Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
         printFile(filePath);
 
+        filePath = Paths.get("C:\\Users\\RS\\IdeaProjects\\OutThere.txt");
+        printFile(filePath);
     }
 
     private static void printFile(Path path) {
